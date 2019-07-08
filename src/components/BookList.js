24208e68d461dc632766f16e3c6db5d63f18e26f
Tim Holt <fremont.holt@gmail.com>
2019-07-08 14:15:25 +0900

    Taking styling out of the render function that was placed there by mistake.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,6 +2,114 @@ import React from "react";
 import Book from "./Book";
 import styled from "styled-components";
 
+const gutter = 20;
+
+const BookListContainer = styled.div`
+  padding: ${gutter}px 0;
+  display: grid;
+  grid-gap: ${gutter}px 0;
+  grid-template-columns: ${gutter}px 1fr ${gutter}px;
+  align-content: start;
+
+  & > * {
+    grid-column: 2 / -2;
+  }
+
+  * > .full {
+    grid-column: 1 / -1;
+  }
+
+  & .scroller {
+    overflow-x: auto;
+    padding: 10px 0;
+    position: relative;
+  }
+`;
+const BookListScrollItem = styled(Book)`
+  grid-column: 2 / -2;
+`;
+
+const BookListScroller = styled.div`
+  display: grid;
+  grid-gap: calc(${gutter}px / 2);
+  grid-template-columns: 10px;
+  grid-template-rows: minmax(150px, 1fr);
+  grid-auto-flow: column;
+
+  overflow-x: scroll;
+  scroll-snap-type: x proximity;
+  padding-bottom: calc(0.75 * ${gutter}px);
+  margin-bottom: calc(-0.25 * ${gutter}px);
+
+  &:before,
+  &:after {
+    content: "";
+    width: 10px;
+  }
+`;
+
+const ScrollButton = styled.div`
+  position: absolute;
+  opacity: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 10;
+  height: 210px;
+  background-color: #555;
+  top: ${gutter / 2}px;
+
+  @media (min-width: 800px) {
+    height: 342px;
+  }
+
+  &.left {
+    left: 20px;
+    & .arrow {
+      transform: rotate(-135deg);
+    }
+  }
+
+  &.right {
+    right: 0;
+    & .arrow {
+      transform: rotate(45deg);
+    }
+  }
+
+  &:hover {
+    opacity: 0.7;
+  }
+
+  & .arrow {
+    position: relative;
+    display: inline-block;
+    vertical-align: middle;
+    color: #fff;
+    box-sizing: border-box;
+    width: 20px;
+    height: 20px;
+    border-width: 4px 4px 0 0;
+    border-style: solid;
+    margin: 10px;
+    &:after,
+    &:before {
+      content: "";
+      box-sizing: border-box;
+    }
+    &:before {
+      right: 0;
+      top: -3px;
+      position: absolute;
+      height: 4px;
+      box-shadow: inset 0 0 0 32px;
+      transform: rotate(-45deg);
+      width: 23px;
+      transform-origin: right top;
+    }
+  }
+`;
+
 class BookList extends React.Component {
   constructor(props) {
     super(props);
@@ -36,116 +144,7 @@ class BookList extends React.Component {
     this.scroller.current.scrollLeft = newPosition;
   }
 
-  render() {
-    const gutter = 20;
-
-    const BookListContainer = styled.div`
-      padding: ${gutter}px 0;
-      display: grid;
-      grid-gap: ${gutter}px 0;
-      grid-template-columns: ${gutter}px 1fr ${gutter}px;
-      align-content: start;
-
-      & > * {
-        grid-column: 2 / -2;
-      }
-
-      * > .full {
-        grid-column: 1 / -1;
-      }
-
-      & .scroller {
-        overflow-x: auto;
-        padding: 10px 0;
-        position: relative;
-      }
-    `;
-    const BookListScrollItem = styled(Book)`
-      grid-column: 2 / -2;
-    `;
-
-    const BookListScroller = styled.div`
-      display: grid;
-      grid-gap: calc(${gutter}px / 2);
-      grid-template-columns: 10px;
-      grid-template-rows: minmax(150px, 1fr);
-      grid-auto-flow: column;
-
-      overflow-x: scroll;
-      scroll-snap-type: x proximity;
-      padding-bottom: calc(0.75 * ${gutter}px);
-      margin-bottom: calc(-0.25 * ${gutter}px);
-
-      &:before,
-      &:after {
-        content: "";
-        width: 10px;
-      }
-    `;
-
-    const ScrollButton = styled.div`
-      position: absolute;
-      opacity: 0;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      z-index: 10;
-      height: 210px;
-      background-color: #555;
-      top: ${gutter / 2}px;
-
-      @media (min-width: 800px) {
-        height: 342px;
-      }
-
-      &.left {
-        left: 20px;
-        & .arrow {
-          transform: rotate(-135deg);
-        }
-      }
-
-      &.right {
-        right: 0;
-        & .arrow {
-          transform: rotate(45deg);
-        }
-      }
-
-      &:hover {
-        opacity: 0.7;
-      }
-
-      & .arrow {
-        position: relative;
-        display: inline-block;
-        vertical-align: middle;
-        color: #fff;
-        box-sizing: border-box;
-        width: 20px;
-        height: 20px;
-        border-width: 4px 4px 0 0;
-        border-style: solid;
-        margin: 10px;
-        &:after,
-        &:before {
-          content: "";
-          box-sizing: border-box;
-        }
-        &:before {
-          right: 0;
-          top: -3px;
-          position: absolute;
-          height: 4px;
-          box-shadow: inset 0 0 0 32px;
-          transform: rotate(-45deg);
-          width: 23px;
-          transform-origin: right top;
-        }
-      }
-    `;
-
-    const books = this.props.books.map(book => (
+  render() {    const books = this.props.books.map(book => (
       <BookListScrollItem book={book} key={book.id} />
     ));
 
